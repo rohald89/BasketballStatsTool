e3423311d6225bbbf00873cbbaad4c14264026d0
Rohald van Merode <rohald_89@live.nl>
2021-10-06 17:33:45 +0200

    created the balance_teams function to split the players into teams

--- app.py
+++ app.py
@@ -7,14 +7,27 @@ def clean_data():
     and change the experience to be a boolean
     '''
     players = constants.PLAYERS
+
     for player in players:
-        player['height'] = int(player['height'][0:2])
+        player['height'] = int(player['height'][:2])
         if player['experience'] == 'YES':
             player['experience'] = True
         else:
             player['experience'] = False
+
     return players
+
+def balance_teams(players):
+    balanced_teams = {}
+    for team in constants.TEAMS:
+        balanced_teams[team] = []
+    while len(players) > 0:
+        for team in balanced_teams:
+            player = players.pop()
+            balanced_teams[team].append(player)
+    print(balanced_teams)
 
 
 if __name__ == '__main__':
-    clean_data()
+    player_data = clean_data()
+    balance_teams(player_data)
